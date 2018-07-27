a30128156237a3b667792b4312197e7c71df7217
vinicius-araujo <vinisergio@gmail.com>
2018-07-27 19:04:24 +0200

    css and carousel's adjustment

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-console.log("entry point working");
 import style from "./index.scss";
+import { UltimateCarousel } from './script/carousel';
+const itemList = [{
+  description: "Craze is one of the most great things in the World",
+  author: "Vinicius"
+}, {
+  description: "Alguma coisa Muito legal",
+  author: "Vinicius Araujo"
+}]
+var taskCarousel = new UltimateCarousel({
+  items: itemList,
+  parent: document.getElementsByClassName('carousel'),
+  content: document.getElementsByClassName('carousel-content'),
+  height: 180
+});
+taskCarousel.init();
